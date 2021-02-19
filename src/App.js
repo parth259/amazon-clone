4234acda919ecdfc91ed91ebcbd418ab65299b83
Parth Patel <68620884+parth259@users.noreply.github.com>
2021-02-18 22:36:29 -0400

    Added products

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
 import "./Header.css";
+import Product from "./Product";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Home />
+      <Product />
     </div>
   );
 }
